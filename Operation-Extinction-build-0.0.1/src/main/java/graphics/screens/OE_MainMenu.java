0a8830076ada6422b244e91bca820581d72d9fe7
toasted-almond <kyle.morris@uconn.edu>
2020-01-13 14:14:16 -0500

    Updated MainMenu

--- Operation-Extinction-build-0.0.1/src/main/java/graphics/screens/OE_MainMenu.java
+++ Operation-Extinction-build-0.0.1/src/main/java/graphics/screens/OE_MainMenu.java
@@ -8,14 +8,11 @@
 */
 package graphics.screens;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
-import java.awt.image.BufferedImage;
 
 import graphics.OE_ScreenConstants;
 /**
@@ -66,7 +63,7 @@ public class OE_MainMenu extends JFrame implements Menu {
 
         // ================================ GENERATE PANELS
         // main panel setup
-        String pathToImage = (_root.concat("/src/oegrafx/screens/graphics/mainBackground.jpg"));
+        String pathToImage = (_root.concat("/src/main/resources/graphics/mainBackground.jpg"));
         _mainPanel = new OE_GraphicPane(pathToImage);
 
         // panel for main buttons
@@ -94,7 +91,7 @@ public class OE_MainMenu extends JFrame implements Menu {
         _buttonPane.add(_quit, gConst);
 
         // panel for random card
-        pathToImage = (_root.concat("/src/oegrafx/screens/graphics/cardTest2.png"));
+        pathToImage = (_root.concat("/src/main/resources/graphics/cardTest2.png"));
         _randCardPane = new OE_GraphicPane(pathToImage);
         //_randCardPane = new OE_GraphicPane(pathToImage);
         _randCardPane.setPreferredSize(new Dimension(180, 229));
@@ -102,12 +99,12 @@ public class OE_MainMenu extends JFrame implements Menu {
         // 163 x 229    original size / 4
 
         // panel for game updates
-        pathToImage = (_root.concat("/src/oegrafx/screens/graphics/updateTest.png"));
+        pathToImage = (_root.concat("/src/main/resources/graphics/updateTest.png"));
         _updatePane = new OE_GraphicPane(pathToImage);
         _updatePane.setPreferredSize(new Dimension(180, 100));
 
         // label for icon
-        pathToImage = (_root.concat("/src/oegrafx/screens/graphics/logo.png"));
+        pathToImage = (_root.concat("/src/main/resources/graphics/logo.png"));
         _logo = new JLabel(new ImageIcon(pathToImage));
         _logo.setOpaque(false);
 
