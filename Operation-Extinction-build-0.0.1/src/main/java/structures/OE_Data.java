eb624a49652f73f741704c14cadd73b16bdeebcf
toasted-almond <kyle.morris@uconn.edu>
2020-03-02 20:14:31 -0500

    OE_Data interface added (03/02/2020)

---
+++ Operation-Extinction-build-0.0.1/src/main/java/structures/OE_Data.java
@@ -0,0 +1,27 @@
+/**
+*===================   OPERATION EXTINCTION   ======================
+*===================       DO NOT ALTER       ======================
+* @author Kyle M. Morris
+* @version 0.0.1
+*
+*
+*/
+package structures;
+/**
+ * Interface for generic data.
+ * Has 2 standard methods, getSig() and getHashedSig().
+ * Every datatype will have this functionality.
+ * transmit() will be used by most other objects that manipulate
+ * the data.
+ * 
+ * Current datatypes: OE_userData, OE_abstractCard
+ * 
+ * @author Kyle M. Morris
+ * @since 0.0.1
+ *
+ */
+public interface OE_Data {
+	public String getSig();
+	public String getHashedSig();
+	public void transmit();
+}
