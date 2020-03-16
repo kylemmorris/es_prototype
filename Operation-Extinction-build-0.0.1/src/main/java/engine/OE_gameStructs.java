6644631c93a8a045cb1a3fa3f9276e76996fa3d4
toasted-almond <kyle.morris@uconn.edu>
2020-03-15 20:20:58 -0400

    Added engine subsystem (03/15/2020)

---
+++ Operation-Extinction-build-0.0.1/src/main/java/engine/OE_gameStructs.java
@@ -0,0 +1,22 @@
+/**
+*===================   OPERATION EXTINCTION   ======================
+*===================       DO NOT ALTER       ======================
+* @author Kyle M. Morris
+* @version 0.0.1
+*
+*
+*/
+
+package engine;
+import javax.swing.JFrame;
+import graphics.screens.OE_StartMenu;
+import structures.OE_GameConstants;
+
+/**
+ * Main data structure wrapper object for the main data structures.
+ * 
+ * @author Kyle M. Morris
+ * @since 0.0.1
+ */
+public class OE_gameStructs {
+}
